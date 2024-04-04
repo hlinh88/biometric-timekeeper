417bfdd54511ef4792cda085c1a23a1ef318f705
Luke Nguyen <luke@outcubator.com>
2024-04-04 22:31:08 +0700

    Initial Commit

---
+++ BiometricTimekeeper/BiometricTimekeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricTimekeeperApp.swift
+//  BiometricTimekeeper
+//
+//  Created by Luke Nguyen on 04/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BiometricTimekeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
